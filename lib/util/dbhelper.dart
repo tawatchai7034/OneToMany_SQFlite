28d77ccbeb6cc37e1535eb751626250eb7a94e5c
tawatchai7034 <tawatchaisunanta@gmail.com>
2022-03-13 18:35:29 +0700

    m

--- lib/util/dbhelper.dart
+++ lib/util/dbhelper.dart
@@ -32,7 +32,7 @@ class DbHelper {
       db = await openDatabase(join(await getDatabasesPath(), 'shopping.db'),
           onCreate: (database, version) {
             database.execute(
-                'CREATE TABLE lists(id INTEGER PRIMARY KEY, name TEXT, priority INTEGER)');
+                'CREATE TABLE lists(id INTEGER PRIMARY KEY, name TEXT, gender TEXT,species TEXT,)');
             database.execute(
                 'CREATE TABLE items(id INTEGER PRIMARY KEY, idList INTEGER, name TEXT, quantity TEXT, note TEXT, ' +
                     'FOREIGN KEY(idList) REFERENCES lists(id))');
